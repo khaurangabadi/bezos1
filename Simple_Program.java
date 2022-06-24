d7ee01783609a9485e8f1719c5530c3c6066b712
khaurangabadi <106445220+khaurangabadi@users.noreply.github.com>
2022-06-24 12:12:06 +0530

    Add files via upload

---
+++ Simple_Program.java
@@ -0,0 +1,16 @@
+//import java.lang.*;
+class Simple_Program
+	{
+     		public static void main(String []args)
+    		{
+
+
+                 System.out.println("hello java language");
+		 System.out.println("This is our first program");
+		System.out.println("Printing our program");
+
+               }
+                   
+			
+
+	}
